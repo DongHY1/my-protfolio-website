0c4edd6fc1688ce3178084589845e8b5b146b7d4
donghy1 <915110101@qq.com>
2022-04-01 16:41:32 +0800

    ✨新增(Vue): 实现点击方块颜色发生变化

--- src/views/Drum.vue
+++ src/views/Drum.vue
@@ -8,7 +8,7 @@
                 <i class="fas fa-random"></i>
             </button>
             <div class="play">
-                <button>
+                <button @click="playHandler">
                     <i class="fas" :class="{ 'fa-play': !isPlaying, 'fa-pause': isPlaying }"></i>
                 </button>
             </div>
@@ -30,22 +30,64 @@
             <div class="timeline"></div>
             <div class="drum set" v-show="tab === 0">
                 <div class="kick">
-                    <div v-for="i in 16" :key="`kick_${i-1}`" class="item" ></div>
+                    <div
+                        v-for="i in 16"
+                        :key="`kick_${i - 1}`"
+                        class="item"
+                        :class="{ 'active': !!sequence.drum.kick[i - 1] }"
+                        @mousedown="clickHandle(sequence.drum.kick, i - 1)"
+                        @touchstart="clickHandle(sequence.drum.kick, i - 1)"
+                    ></div>
                 </div>
-                <div class="kick">
-                    <div v-for="i in 16" class="item"></div>
+                <div class="hihat">
+                    <div
+                        v-for="i in 16"
+                        class="item"
+                        :key="`hihat_${i - 1}`"
+                        :class="{ 'active': !!sequence.drum.hihat[i - 1] }"
+                        @mousedown="clickHandle(sequence.drum.hihat, i - 1)"
+                        @touchstart="clickHandle(sequence.drum.hihat, i - 1)"
+                    ></div>
                 </div>
-                <div class="kick">
-                    <div v-for="i in 16" class="item"></div>
+                <div class="snare">
+                    <div
+                        v-for="i in 16"
+                        class="item"
+                        :key="`snare_${i - 1}`"
+                        :class="{ 'active': !!sequence.drum.snare[i - 1] }"
+                        @mousedown="clickHandle(sequence.drum.snare, i - 1)"
+                        @touchstart="clickHandle(sequence.drum.snare, i - 1)"
+                    ></div>
                 </div>
-                <div class="kick">
-                    <div v-for="i in 16" class="item"></div>
+                <div class="tomL">
+                    <div
+                        v-for="i in 16"
+                        class="item"
+                        :key="`tomL_${i - 1}`"
+                        :class="{ 'active': !!sequence.drum.tomL[i - 1] }"
+                        @mousedown="clickHandle(sequence.drum.tomL, i - 1)"
+                        @touchstart="clickHandle(sequence.drum.tomL, i - 1)"
+                    ></div>
                 </div>
-                <div class="kick">
-                    <div v-for="i in 16" class="item"></div>
+                <div class="tomM">
+                    <div
+                        v-for="i in 16"
+                        class="item"
+                        :key="`tomL_${i - 1}`"
+                        :class="{ 'active': !!sequence.drum.tomM[i - 1] }"
+                        @mousedown="clickHandle(sequence.drum.tomM, i - 1)"
+                        @touchstart="clickHandle(sequence.drum.tomM, i - 1)"
+                    ></div>
                 </div>
-                <div class="kick">
-                    <div v-for="i in 16" class="item"></div>
+                <div class="tomH">
+                    <div
+                        v-for="i in 16"
+                        class="item"
+                        :key="`tomL_${i - 1}`"
+                        :class="{ 'active': !!sequence.drum.tomH[i - 1] }"
+                        @mousedown="clickHandle(sequence.drum.tomH, i - 1)"
+                        @touchstart="clickHandle(sequence.drum.tomH, i - 1)"
+                    ></div>
                 </div>
             </div>
             <div class="lead set" v-show="tab === 1"></div>
@@ -60,21 +102,24 @@ import Snare from '@/lib/snare.js'
 let tab = ref(0);
 let isPlaying = ref(false)
 let BPM = ref(120)
-// 
-const defaultSequencer = reactive({
+let sequence = reactive({
     drum: {
         kick: getEmptyArray(),
         hihat: getEmptyArray(),
         snare: getEmptyArray(),
         tomL: getEmptyArray(),
         tomM: getEmptyArray(),
-        tomH: getEmptyArray()
-    },
-    lead: Array.from({ length: 7 }, getEmptyArray)
+        tomH: getEmptyArray(),
+    }
 })
+// 处理点击事件
+// kick 数组的第n个元素被点击
+function clickHandle(arr, i) {
+    arr[i] = !arr[i]
+}
 // 默认创建长度为16的空数组
-function getEmptyArray(length=16){
-    return Array.from({length},()=>0)
+function getEmptyArray(length = 16) {
+    return Array.from({ length }, () => 0)
 }
 // 建立乐器
 const kick = new Tone.MembraneSynth({
@@ -117,6 +162,30 @@ tomL.volume.value = 0
 tomM.volume.value = 0
 tomH.volume.value = 0
 // poly.set('volume', 2)
+//建立播放系统
+Tone.Transport.bpm.value = BPM.value
+Tone.Transport.scheduleRepeat((time)=>{
+console.log(time)
+if(sequence.drum.kick[i])kick.triggerAttackRelease('C2', '4n', time)
+},'16n')
+
+
+// 点击开始 可以播放
+function playHandler() {
+    if (isPlaying.value) {
+        stop()
+    } else {
+        play()
+    }
+}
+function play() {
+    isPlaying.value = true
+    Tone.Transport.start()
+}
+function stop() {
+    isPlaying.value = false
+    Tone.Transport.stop()
+}
 </script>
 
 <style lang="scss" scoped>
@@ -139,7 +208,12 @@ tomH.volume.value = 0
     .pad {
         .set {
             margin: 5px 0;
-            .kick {
+            .kick,
+            .hihat,
+            .snare,
+            .tomL,
+            .tomM,
+            .tomH {
                 display: flex;
                 flex-wrap: nowrap;
                 justify-content: flex-start;
@@ -152,6 +226,9 @@ tomH.volume.value = 0
                     border-radius: 10px;
                     flex: 1;
                     background-color: white;
+                }
+                .active {
+                    background-color: red;
                 }
             }
         }
