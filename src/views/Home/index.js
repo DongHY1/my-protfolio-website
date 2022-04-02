fbaefa660811955e618f9c6680011a7b05891d35
donghy1 <915110101@qq.com>
2022-04-02 22:07:46 +0800

    ✨新增(javascript): 添加地球跟随鼠标移动效果,将sphere加入到group中

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -2,8 +2,8 @@ import * as THREE from "three";
 import earth from "@/assets/Home/earth-4k.jpg";
 import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
-import atmosphereVertexShader from "./shaders/atmosphereVertex.glsl"
-import atmosphereFragmentShader from "./shaders/atmosphereFragment.glsl"
+import atmosphereVertexShader from "./shaders/atmosphereVertex.glsl";
+import atmosphereFragmentShader from "./shaders/atmosphereFragment.glsl";
 export function initThree() {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
@@ -36,23 +36,35 @@ export function initThree() {
       // map: new THREE.TextureLoader().load(earth),
     })
   );
-  scene.add(sphere);
   //   创建大气层
   const atmosphere = new THREE.Mesh(
     new THREE.SphereGeometry(5, 50, 50),
     new THREE.ShaderMaterial({
-      vertexShader:atmosphereVertexShader,
-      fragmentShader:atmosphereFragmentShader,
-      blending:THREE.AdditiveBlending,
-      side:THREE.BackSide
+      vertexShader: atmosphereVertexShader,
+      fragmentShader: atmosphereFragmentShader,
+      blending: THREE.AdditiveBlending,
+      side: THREE.BackSide,
     })
   );
-  atmosphere.scale.set(1.1,1.1,1.1)
+  atmosphere.scale.set(1.1, 1.1, 1.1);
   scene.add(atmosphere);
   camera.position.z = 15;
+  const group = new THREE.Group();
+  group.add(sphere);
+  scene.add(group);
+  const mouse = {
+    x: undefined,
+    y: undefined,
+  };
   function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    sphere.rotation.y += 0.001;
+    group.rotation.y = mouse.x*0.5;
   }
   animate();
+  addEventListener("mousemove", (event) => {
+    mouse.x = (event.clientX / innerWidth) * 2 - 1;
+    mouse.y = (event.clientX / innerHeight) * 2 - 1;
+  });
 }
