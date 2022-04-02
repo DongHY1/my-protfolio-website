6dc07179c7cea43f02deb4da551252d1ef986625
donghy1 <915110101@qq.com>
2022-04-02 16:43:41 +0800

    ✨新增(javascript): 添加地球材质贴图

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import earth from '@/assets/Home/earth-4k.jpg'
 export function initThree() {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
@@ -15,7 +16,8 @@ export function initThree() {
   const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(5, 50, 50),
     new THREE.MeshBasicMaterial({
-      color: 0xff0000,
+    //   color: 0xff0000,
+    map:new THREE.TextureLoader().load(earth)
     })
   );
   scene.add(sphere);
