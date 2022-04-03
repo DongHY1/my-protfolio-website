9277ed364187ba72d1efd77ac70823df96c78623
donghy1 <915110101@qq.com>
2022-04-03 13:58:38 +0800

    ✨新增(javascript): 根据国家坐标渲染出圆点

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -4,7 +4,7 @@ import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
 import atmosphereVertexShader from "./shaders/atmosphereVertex.glsl";
 import atmosphereFragmentShader from "./shaders/atmosphereFragment.glsl";
-import gsap from 'gsap'
+import gsap from "gsap";
 export function initThree() {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
@@ -54,18 +54,27 @@ export function initThree() {
   group.add(sphere);
   scene.add(group);
   // 星空背景
-  const starVertices = []
-  for(let i=0;i<10000;i++){
-    const x = (Math.random()-0.5)*2000
-    const y = (Math.random()-0.5)*2000
-    const z = -(Math.random())*1000
-    starVertices.push(x,y,z)
+  const starVertices = [];
+  for (let i = 0; i < 10000; i++) {
+    const x = (Math.random() - 0.5) * 2000;
+    const y = (Math.random() - 0.5) * 2000;
+    const z = -(Math.random() - 0.5) * 2000;
+    starVertices.push(x, y, z);
   }
-  const starGeometry = new THREE.BufferGeometry()
-  starGeometry.setAttribute('position',new THREE.Float32BufferAttribute(starVertices,3))
-  const starMeterial = new THREE.PointsMaterial({color:0xffffff})
-  const stars = new THREE.Points(starGeometry,starMeterial)
-  scene.add(stars)
+  const starGeometry = new THREE.BufferGeometry();
+  starGeometry.setAttribute(
+    "position",
+    new THREE.Float32BufferAttribute(starVertices, 3)
+  );
+  const starMeterial = new THREE.PointsMaterial({ color: 0xffffff });
+  const stars = new THREE.Points(starGeometry, starMeterial);
+  scene.add(stars);
+  // 添加地点
+  createPoint(35,103,5,group)
+  createPoint(-14,-52,5,group)
+  createPoint(20,78,5,group)
+  // 旋转地球正确角度
+  sphere.rotation.y = -Math.PI/2
   const mouse = {
     x: undefined,
     y: undefined,
@@ -73,17 +82,44 @@ export function initThree() {
   function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    sphere.rotation.y += 0.001;
+    // sphere.rotation.y += 0.001;
     // group.rotation.y = mouse.x*0.5;
-    gsap.to(group.rotation,{
-      x:-mouse.y*0.3,
-      y:mouse.x*0.5,
-      duration:2
-    })
+    gsap.to(group.rotation, {
+      x: -mouse.y * 1.3,
+      y: mouse.x * 1.5,
+      duration: 2,
+    });
   }
   animate();
   addEventListener("mousemove", (event) => {
     mouse.x = (event.clientX / innerWidth) * 2 - 1;
     mouse.y = (event.clientX / innerHeight) * 2 - 1;
   });
+}
+/**
+ *
+ * @param {*} latitude 纬度 N为正
+ * @param {*} longitude 经度 E为正
+ * @param {*} radius 球体半径
+ * @param {*} group 分组
+ */
+function createPoint(latitude, longitude,radius,group) {
+  const point = new THREE.Mesh(
+    new THREE.SphereGeometry(0.1, 50, 50),
+    new THREE.MeshBasicMaterial({
+      color: "#ff0000",
+    })
+  );
+  // 坐标转换
+  const transformLatitude = (latitude / 180) * Math.PI;
+  const transformLongitude = (longitude / 180) * Math.PI;
+  const x = radius * Math.cos(transformLatitude) * Math.sin(transformLongitude);
+  const y = radius * Math.sin(transformLatitude);
+  const z = radius * Math.cos(transformLatitude) * Math.cos(transformLongitude);
+  // 更新位置
+  point.position.x = x
+  point.position.y = y
+  point.position.z = z
+  // 添加坐标
+  group.add(point)
 }
