d72c12f91fabe6c3de1de3757f49e9d2c7942113
donghy1 <915110101@qq.com>
2022-04-04 20:21:13 +0800

    ⚡️性能(javascript): 采用cdn

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { visualizer } from "rollup-plugin-visualizer";
 import viteCompression from "vite-plugin-compression";
+import importCDN from 'vite-plugin-cdn-import'
 export default defineConfig({
   base: "./",
   plugins: [
@@ -11,6 +12,25 @@ export default defineConfig({
     vitePluginString(),
     visualizer(),
     viteCompression(),
+    importCDN({
+      modules:[
+        {
+          name:'vue',
+          var:'Vue',
+          path:`https://cdn.bootcdn.net/ajax/libs/vue/3.2.31/vue.runtime.global.prod.min.js`
+        },
+        {
+          name:'vue-router',
+          var:'VueRouter',
+          path:`https://cdn.bootcdn.net/ajax/libs/vue-router/4.0.14/vue-router.global.prod.min.js`
+        },
+        {
+          name:'tone',
+          var:'Tone',
+          path:`https://cdn.bootcdn.net/ajax/libs/tone/14.8.38/Tone.min.js`
+        }
+      ]
+    })
   ],
   resolve: {
     alias: {
