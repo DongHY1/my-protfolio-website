ead33c52924664c7c6ff5c233983ef50c81174d7
donghy1 <915110101@qq.com>
2022-04-22 19:11:10 +0800

    🐛修复(Vue): 新增404页面
    ISSUES CLOSED: 、

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,16 @@ const router = createRouter({
       path: "/home",
       component: ()=>import("../views/Home.vue"),
     },
+    {
+      path: '/404',
+      name: 'NotFound',
+      component: () => import('../views/NotFound.vue')
+    },
+    // 所有未定义路由，全部重定向到404页
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/404'
+    }
   ],
 });
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="notfound">
+        <h1 class="text" @click="jumpHome">页面无法访问，点击我跳转到首页吧~</h1>
+    </div>
+</template>
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+function jumpHome() {
+    router.replace('/home')
+}
+</script>
+<style lang="scss">
+.notfound {
+    width: 100vw;
+    height: 40vh;
+    margin-top: 100px;
+    .text {
+        text-align: center;
+        color: white;
+        cursor: pointer;
+    }
+
+}
+</style>
